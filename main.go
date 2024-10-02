d6ddec95705a7588f8d10989def073923bc40fa7
Vasek - Tom C <tom.chauveau@epitech.eu>
2024-10-02 12:18:04 -0700

    a
    Signed-off-by: Vasek - Tom C <tom.chauveau@epitech.eu>

--- main.go
+++ main.go
@@ -57,7 +57,7 @@ func (m *InstantModule) CompileBun(ctx context.Context, source *dagger.Directory
 		WithWorkdir("/src").
 		WithExec([]string{"bun", "install"}).
 		WithExec([]string{"bun", "build", "src/entrypoint.ts", "--target", "bun", "--outdir", "dist"}).
-		WithEntrypoint([]string{"bun", "run", "/dist/entrypoint.js"}).
+		WithEntrypoint([]string{"bun", "run", "./dist/entrypoint.js"}).
 		WithoutFile("src/entrypoint.ts").
 		Publish(ctx, "ttl.sh/dagger/instant-module/bun/v0")
 }
--- test/typescript/main.go
+++ test/typescript/main.go
@@ -10,7 +10,7 @@ type TestInstant struct{
 }
 
 func (t *TestInstant) ModuleRuntime(ctx context.Context, modSource *dagger.ModuleSource, introspectionJSON *dagger.File) (*dagger.Container, error) {
-	return dag.Container().From("ttl.sh/dagger/instant-module/bun/v0@sha256:b2847c3f8cf3d1419da93bb55682620f65d5f52d83d596926f676fb18e53a3c6"), nil
+	return dag.Container().From("ttl.sh/dagger/instant-module/bun/v0@sha256:328113ab597fecd7c5d26bc0ed930f1cf9b9972307450bba6335c7cecac6a0b3"), nil
 }
 
 func (t *TestInstant) Codegen(ctx context.Context, modSource *dagger.ModuleSource, introspectionJSON *dagger.File) (*dagger.GeneratedCode, error) {
