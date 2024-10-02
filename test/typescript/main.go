9ff17163470a17f138980ce00a407d278795fde6
Vasek - Tom C <tom.chauveau@epitech.eu>
2024-10-02 12:25:20 -0700

    a
    Signed-off-by: Vasek - Tom C <tom.chauveau@epitech.eu>

--- test/typescript/main.go
+++ test/typescript/main.go
@@ -10,7 +10,7 @@ type TestInstant struct{
 }
 
 func (t *TestInstant) ModuleRuntime(ctx context.Context, modSource *dagger.ModuleSource, introspectionJSON *dagger.File) (*dagger.Container, error) {
-	return dag.Container().From("ttl.sh/dagger/instant-module/bun/v0@sha256:b4acf4f0da5a695c8f8a07c526e43a7aed718359007adcc6d857e188613fe6d4"), nil
+	return dag.Container().From("ttl.sh/dagger/instant-module/bun/v0@sha256:0383491cb26ae290394f6c5bfe115eee811b5360730da18b37ed1c3260570639"), nil
 }
 
 func (t *TestInstant) Codegen(ctx context.Context, modSource *dagger.ModuleSource, introspectionJSON *dagger.File) (*dagger.GeneratedCode, error) {
